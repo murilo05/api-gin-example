48cda70f17f3a28207e88517e1e113766638d7d1
mubitencou <murilo.bitencourt@engdb.com.br>
2023-01-05 04:56:23 -0300

    Fix: unused comments removed

--- main.go
+++ main.go
@@ -33,9 +33,7 @@ func main() {
 		TIMEOUT:     TIMEOUT,
 	}
 
-	//testar api, mudar tempo ctx, documentação, teste unitr
-
 	infrastructure.Config(env, r)
 
-	r.Run(HOST + ":" + PORT) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(HOST + ":" + PORT)
 }
